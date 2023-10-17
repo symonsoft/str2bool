51e1b466853fba5e764b74bc7474231096d7103c
Sam Fakhreddine <sfakhreddine@onica.com>
2023-10-17 13:53:08 -0600

    Update __init__.py

--- str2bool/__init__.py
+++ str2bool/__init__.py
@@ -1,21 +1,22 @@
-import sys
+from typing import Union
 
-_true_set = {'yes', 'true', 't', 'y', '1'}
-_false_set = {'no', 'false', 'f', 'n', '0'}
+# Refactored Python 3.9 compatible code for string to boolean conversion
 
-
-def str2bool(value, raise_exc=False):
-    if isinstance(value, str) or sys.version_info[0] < 3 and isinstance(value, basestring):
+def str2bool(value: Union[str, None], raise_exc: bool = False) -> Union[bool, None]:
+    true_set = {'yes', 'true', 't', 'y', '1'}
+    false_set = {'no', 'false', 'f', 'n', '0'}
+    
+    if isinstance(value, str):
         value = value.lower()
-        if value in _true_set:
+        if value in true_set:
             return True
-        if value in _false_set:
+        if value in false_set:
             return False
-
+    
     if raise_exc:
-        raise ValueError('Expected "%s"' % '", "'.join(_true_set | _false_set))
+        raise ValueError(f'Expected one of: {", ".join(true_set | false_set)}')
+    
     return None
 
-
-def str2bool_exc(value):
+def str2bool_exc(value: Union[str, None]) -> bool:
     return str2bool(value, raise_exc=True)
